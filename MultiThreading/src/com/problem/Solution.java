2a7c1f05b75ce5b55ad398daf9fe61fb7a1e12c7
Kumar <mohitnevatru@gmail.com>
2022-01-01 23:52:41 +0530

    java project on multithreading

---
+++ MultiThreading/src/com/problem/Solution.java
@@ -0,0 +1,105 @@
+package com.problem;
+
+import java.util.Scanner;
+
+class Task1 extends Thread {
+	static int a;
+	static int beg;
+	int start = beg + 2;
+	int end = a + 2;
+	int count = 0;
+
+	public void run() {
+		// System.out.println(a+" "+beg);
+		for (int j = start; j < end; j++) {
+			Solution.threadArray[j] = count;
+			count++;
+		}
+	}
+}
+
+class Task2 extends Thread {
+	static int a;
+	static int beg;
+	int count = beg;
+
+	public void run() {
+		// System.out.println(a+" "+beg);
+		for (int i = 0; i < Task3.beg; i++) {
+			// System.out.println(Main.threadArray[i]);
+			if (Solution.threadArray[i] <= 0 && i != 2) {
+				Solution.threadArray[i] = count++;
+			}
+		}
+	}
+}
+
+class Task3 extends Thread {
+	public static int a;
+	public static int beg;
+
+	public void run() {
+		// System.out.println(a+" "+beg);
+		for (int i = beg; i < a + beg; i++) {
+			Solution.threadArray[i] = i;
+		}
+
+	}
+}
+
+public class Solution {
+	public static final int[] threadArray = new int[300];
+	public static volatile String i = 0 + "";
+
+	public boolean test() throws InterruptedException {
+		Task1 task1 = new Task1();
+		Task2 task2 = new Task2();
+		Task3 task3 = new Task3();
+		Thread task2Thread = new Thread(task2);
+		Thread task3Thread = new Thread(task3);
+		task1.start();
+		task2Thread.start();
+		task1.join();
+		task2Thread.join();
+		task3Thread.start();
+		int first = Task1.a + Task2.a;
+		int containsSecondThread = Task1.a;
+		String oneAndTwo = "";
+		String sizeOfTask1 = "";
+		for (int i = 0; i < first; i++) {
+			oneAndTwo += threadArray[i] + " ";
+		}
+		for (int i = 0; i < containsSecondThread; i++) {
+			sizeOfTask1 += threadArray[i] + " ";
+		}
+		int begOfTask3 = Task3.beg;
+		String checkingString = "";
+		for (int i = begOfTask3; i < threadArray.length; i++) {
+			checkingString += i + " ";
+		}
+		String task3String = "";
+		for (int j = begOfTask3; j < threadArray.length; j++) {
+			task3String += threadArray[j] + " ";
+		}
+		if ((!oneAndTwo.contains(begOfTask3 + "") && sizeOfTask1.contains(Task2.beg + ""))
+				&& task3String.equals(checkingString)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		Scanner sc = new Scanner(System.in);
+		Solution solution = new Solution();
+		int one = sc.nextInt();
+		Task1.a = one;
+		Task1.beg = 0;
+		int two = sc.nextInt();
+		Task2.a = two;
+		Task2.beg = one;
+		int three = sc.nextInt();
+		Task3.a = three;
+		Task3.beg = one + two;
+		System.out.print(solution.test());
+	}
+}
